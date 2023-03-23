ab1b1b769e4971f206aed1614d993d6e2bffdda0
TrinhDucTiep <nhungthisope123@gmail.com>
2023-03-23 10:56:38 +0700

    add add courses function

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug); //add plugin
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxlength: 255 },
-    description: { type: String, maxlength: 600 },
-    image: { type: String, maxlength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    name: { type: String, require: true, },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, require: true },
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true }, //avoid same slug --> error
+}, {
+    timestamps: true, //auto generate
 });
 
 module.exports = mongoose.model('Course', Course);
